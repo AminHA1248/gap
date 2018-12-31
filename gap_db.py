dc1ceeb0ab1b441e9131e19fd0e71a95dab75578
AminHA1248 <amin.h.a@gmail.com>
2018-12-30 21:53:40 -0500

    + Added face, gender and age detector and face alignment

---
+++ gap_db.py
@@ -0,0 +1,81 @@
+import sys
+sys.path.append('../common')
+import tools
+import warnings
+warnings.filterwarnings("ignore")
+
+class Gap_Db():
+    def __init__(self):
+        self._db = self._connectiondb()
+
+    def _connectiondb(self, database='MYSQLDB'):
+        config = tools.read_config_file()
+        params = tools.get_mysql_connection_parameters(config, database)
+        db = tools.connect_mysql(params)
+        return db
+    
+    def get_country_code(self, country, region, lat, lng):
+        cursor = self._db.cursor()
+
+        # update table with new value if country doesn't exist
+        sql = """INSERT IGNORE INTO countries (name, region, lat, lng) VALUES (%s,%s,%s,%s)"""
+        cursor.execute(sql, [country, region, lat, lng])
+        self._db.commit()
+        
+        sql = """SELECT id FROM countries WHERE name="%s" """%country
+        cursor.execute(sql)
+        ret_val = cursor.fetchone()[0]
+        return ret_val
+
+    def update_countries(self, df_countries):
+        # update database
+        cursor = self._db.cursor()
+        if df_countries.shape[1] == 2:
+            sql = "INSERT IGNORE INTO countries (name, region) VALUES (%s,%s)"
+            cursor.executemany(sql, df_countries.values.tolist())
+            self._db.commit()
+        elif df_countries.shape[1] == 4:
+            sql = "INSERT IGNORE INTO countries (name, region, lat, lng) VALUES (%s,%s,%s,%s)"
+            cursor.executemany(sql, df_countries.values.tolist())
+            self._db.commit()
+        else:
+            raise ValueError("countries must contain name, region, [lat], [lng]")
+
+    def update_families(self, image, country_code, income, incomeQuality):
+        cursor = self._db.cursor()
+        sql = """
+        INSERT IGNORE INTO families (`image`, `country`, `income`, `income_quality`) VALUES (%s,%s,%s,%s)
+        """
+        cursor.execute(sql, [image, country_code, income, incomeQuality])
+        self._db.commit()
+
+    def update_family_country(self, country, region, income, incomeQuality, image, lat, lng):
+        country_code = self.get_country_code(country, region, lat, lng)
+        self.update_families(image, country_code, income, incomeQuality)
+
+    def get_family_id(self, image):
+        cursor = self._db.cursor()
+
+        sql = """SELECT id FROM families WHERE image="%s" """%image
+        cursor.execute(sql)
+        ret_val = cursor.fetchone()[0]
+        return ret_val
+
+    def update_faces(self, family, image, age, gender):
+        cursor = self._db.cursor()
+        sql = """
+        INSERT IGNORE INTO faces (`family`, `image`, `age`, `gender`) VALUES (%s,%s,%s,%s)
+        """
+        cursor.execute(sql, [family, image, age, gender])
+        self._db.commit()
+
+    def is_face_done(self, family_id):
+        cursor = self._db.cursor()
+        sql = """SELECT * FROM faces WHERE family=%s"""%family_id
+        cursor.execute(sql)
+        res = cursor.fetchall()
+        if len(res) > 0:
+            is_face_done = True
+        else:
+            is_face_done = False
+        return is_face_done
--- get_data.py
+++ get_data.py
@@ -5,50 +5,93 @@ Created on Sun Nov 18 20:10:20 2018
 
 @author: amin
 """
-
+#%%============================================================================
+# Imports
+# =============================================================================
 import os
 import urllib
 import requests
-from tqdm import tqdm
-
-
-#%%
-def get_url_from_href(url, href):
-    """ This function returns url from href"""
-    new_url = urllib.parse.urljoin(url, href)
-    return new_url
-
-def dl_jpg_from_url(img_url, directory):
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-    os.system("wget -P /home/amin/projects/gapminder/%s '%s'"%(directory, img_url))
-
-def dl_all_links(url, directory):
-    """This function gets a url as input and returns all hyperlinks on the page"""
-    links = [i['background'] for i in requests.get(url).json()['data']['streetPlaces']]
-    for link in tqdm(links):
-        try:
-            img_url = get_url_from_href("https://", link)
-            # print("downloading %s..."%img_url)
-            dl_jpg_from_url(img_url, directory)
-        except:
-            pass
-
-
-#%%
-url = "https://consumer-api-prod.dollarstreet.org/v1/things?lang=en&thing=Families&countries=World&regions=World&zoom=4&mobileZoom=4&row=1&lowIncome=13&highIncome=10813&currency=usd&time=month&resolution=original"
-dl_all_links(url, "families")
-
+import pandas as pd
+from gap_db import Gap_Db
 
 
 #%%============================================================================
-# Tmporary
+# Settings
 # =============================================================================
+pd.set_option('display.max_rows', 10)
+pd.set_option('display.max_columns', 50)
+pd.set_option('display.width', 200)
+
+
+#%%============================================================================
+# Helpers
+# =============================================================================
+class Gap_Scrapper():
+    def __init__(slef):
+        pass
+
+    def get_url_from_href(self, url, href):
+        """ This function returns url from href"""
+        new_url = urllib.parse.urljoin(url, href)
+        return new_url
+    
+    def dl_jpg_from_url(self, img_url, dst_fn):
+        dirname = os.path.dirname(dst_fn)
+        if not os.path.exists(dirname):
+            print("creating directory '%s'..."%dirname)
+            os.makedirs(dirname)
+        if not os.path.exists(dst_fn):
+            print("downloading photo '%s'..."%dst_fn)
+            os.system("""wget -O "%s" "%s" """%(dst_fn, img_url))
+    
+    def http_request(self, url):
+        response = requests.get(url=url, timeout=(3.05, None))
+        res_dict = response.json()
+        return res_dict
+
+    def dl_family_photos(self, url, directory):
+        """This function gets a url as input and returns all hyperlinks on the page"""
+        res = self.http_request(url)
+        families = [i for i in res['data']['streetPlaces']]
+        #with open('families.pkl', 'wb') as fp:
+        #    pickle.dump(families, fp, protocol=-1)
+        #with open('families.pkl', 'rb') as fp:
+        #    families = pickle.load(fp)
+        for family in families:
+            try:
+                image, country, region, income, incomeQuality, lat, lng, link = \
+                family['image'], family['country'], family['region'], \
+                family['income'], family['incomeQuality'], \
+                family['lat'], family['lng'], family['background']
+                
+                # download and save photo
+                dst_fn = os.path.join(directory, image+'.jpg')
+                #link = link.replace("480x480", "original")
+                img_url = self.get_url_from_href("https://", link)
+                self.dl_jpg_from_url(img_url, dst_fn)
+                
+                # update database
+                gap_db.update_family_country(country, region, income, incomeQuality, image, lat, lng)
+    
+            except Exception as e:
+                print(e)
+                pass
+    
+
+
+#%%============================================================================
+# Download family photoes
+# =============================================================================
+# instantiate
+gap_db = Gap_Db()
+gap_scrapper = Gap_Scrapper()
+
 # low-res images
 url = "https://consumer-api-prod.dollarstreet.org/v1/things?lang=en&thing=Families&countries=World&regions=World&zoom=4&mobileZoom=4&row=1&lowIncome=13&highIncome=10813&currency=usd&time=month&resolution=480x480"
 
-# list of countries
-url = "https://consumer-api-prod.dollarstreet.org/v1/countries-filter?lang=en&thing=Families&countries=World&regions=World&zoom=4&mobileZoom=4&row=1&lowIncome=13&highIncome=10813&currency=usd&time=month"
-
 # menu items
 url = "https://consumer-api-prod.dollarstreet.org/v1/things-filter?lang=en&thing=Families&countries=World&regions=World&zoom=4&mobileZoom=4&row=1&lowIncome=13&highIncome=10813&currency=usd&time=month"
+
+# high res images
+url = "https://consumer-api-prod.dollarstreet.org/v1/things?lang=en&thing=Families&countries=World&regions=World&zoom=4&mobileZoom=4&row=1&lowIncome=13&highIncome=10813&currency=usd&time=month&resolution=original"
+gap_scrapper.dl_family_photos(url, directory="/home/amin/projects/gap/families")
